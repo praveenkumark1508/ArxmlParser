23fd1be57aea68b4bc54790f5e0058015a0e9e21
Praveen Kumar K <praveenkumark1508@gmail.com>
2017-10-10 01:41:14 +0530

    First level of parsing implemented and code is running

--- ARParser/arparser.py
+++ ARParser/arparser.py
@@ -6,64 +6,42 @@ This module gives data model of comunication details in an arxml.
 
 from lxml import etree
 
-namespaces = {'a':'http://autosar.org/3.2.3',
-              'b':'http://www.w3.org/2001/XMLSchema-instance'}
-
-def parse_element(parse_func):
-    '''
-    Decorator for parse functions
-    '''
-    def wrapper(obj, element):
-        '''
-        Wrapper to parse all the child elements using the parse_func
-        '''
-        if element.tag in obj.parse_list:
-            parse_func(obj, element)
-        else:
-            for child in element:
-                wrapper(obj, child)
-
-    return wrapper
-
 class BaseParser:
     '''
     Base class for parsers
     '''
-    parse_list = {'{'+namespaces['a']+'}'+'SHORT-NAME':'name',
-                  '{'+namespaces['a']+'}'+'FRAME-LENGTH':'length',
-                  '{'+namespaces['a']+'}'+'LENGTH':'length'}
-
-    def __init__(self, element, datamodel):
+    def __init__(self, element, datamodel, nsmap):
         self.element = element
         self.datamodel = datamodel
+        self.nsmap = nsmap
+        self.parse_dict = {'{'+self.nsmap['default']+'}'+'SHORT-NAME':'name',
+                           '{'+self.nsmap['default']+'}'+'FRAME-LENGTH':'length',
+                           '{'+self.nsmap['default']+'}'+'LENGTH':'length',
+                           '{'+self.nsmap['default']+'}'+'L-2':'description'}
 
     def getname(self):
         '''
         Get the name of the container
         '''
-        return self.element.find('./a:SHORT-NAME', namespaces)
+        return self.element.find('./default:SHORT-NAME', self.nsmap).text
 
-    def setattribute(self, childelement):
+    def setattribute(self, childelement, datamodel):
         '''
         Set the attributes of the container
         '''
-        self.datamodel[self.parse_list[childelement.tag]] = childelement.text
+        datamodel[self.parse_dict[childelement.tag]] = childelement.text
 
-    def parse(self):
-        '''
-        Parse the element
-        '''
-        self.parse_children(self.element)
+    def parse_children(self, attribkey):
+        datamodel = DataModel()
 
-    @parse_element
-    def parse_children(self, element):
-        '''
-        Parsing function
-        '''
-        try:
-            self.parse_list[element.tag](element)
-        except TypeError:
-            self.setattribute(element)
+        for subelement in self.element.iterchildren():
+            if subelement.tag in self.parse_dict:
+                try:
+                    self.parse_dict[subelement.tag](subelement)
+                except TypeError:
+                    self.setattribute(subelement, datamodel)
+
+        self.datamodel[attribkey].append(datamodel['name'], datamodel)
 
 class DataModel(dict):
     '''
@@ -78,51 +56,129 @@ class DataModel(dict):
     def __getattr__(self, attr):
         return self.__getitem__(attr)
 
+    def __repr__(self):
+        string = '\n'.join([key for key in self.keys()])
+        return string
+
+    def __str__(self):
+        string = '\n'.join([key for key in self.keys()])
+        return string
+
+    def append(self, name, datamodel):
+        '''
+        Add the member if the member is not already present. If the member is
+        already present, then update its value.
+        '''
+        if not name in self:
+            self[name] = datamodel
+        else:
+            self[name].update(datamodel)
+
 class ISignalParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('signal')
 
 class SystemSignalParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('signal')
 
 class CompuMethodParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('compumethods')
 
 class IPduParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('pdus')
 
 class FrameParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('frames')
 
 class PhysicalChannelParser(BaseParser):
-    pass
+    def parse(self):
+        '''
+        Parse the element
+        '''
+        self.parse_children('networks')
 
 class MainParser:
     '''
     Manager class for parsing the arxml
     '''
-    parse_list = {'{'+namespaces['a']+'}'+'I-SIGNAL':ISignalParser,
-                  '{'+namespaces['a']+'}'+'SYSTEM-SIGNAL':SystemSignalParser,
-                  '{'+namespaces['a']+'}'+'COMPU-METHOD':CompuMethodParser,
-                  '{'+namespaces['a']+'}'+'SIGNAL-I-PDU':IPduParser,
-                  '{'+namespaces['a']+'}'+'FRAME':FrameParser,
-                  '{'+namespaces['a']+'}'+'PHYSICAL-CHANNEL':PhysicalChannelParser}
-
     def __init__(self, filename):
         self.filename = filename
         self.xmldoc = etree.parse(self.filename)
         self.root = self.xmldoc.getroot()
+        self.nsmap = self.get_nsmap()
+        self.arxmlversion = self.checkarxml()
         self.datamodel = DataModel()
+        self.parse_dict = {'{'+self.nsmap['default']+'}'+'I-SIGNAL':ISignalParser,
+                           '{'+self.nsmap['default']+'}'+'SYSTEM-SIGNAL':SystemSignalParser,
+                           '{'+self.nsmap['default']+'}'+'COMPU-METHOD':CompuMethodParser,
+                           '{'+self.nsmap['default']+'}'+'SIGNAL-I-PDU':IPduParser,
+                           '{'+self.nsmap['default']+'}'+'FRAME':FrameParser,
+                           '{'+self.nsmap['default']+'}'+'PHYSICAL-CHANNEL':PhysicalChannelParser}
 
     def parse(self):
         '''
         Parse the complete arxml
         '''
-        self.parse_children(self.root)
+        self.prepare_datamodel()
+        self.parse_children(self.root, self.datamodel)
+        return self.datamodel
 
-    @parse_element
-    def parse_children(self, element):
+    def parse_children(self, element, datamodel):
         '''
         Parse function for Manager class
         '''
-        parser = self.parse_list[element.tag](element, self.datamodel)
-        parser.parse()
+        if element.tag in self.parse_dict:
+            parser = self.parse_dict[element.tag](element, datamodel, self.nsmap)
+            parser.parse()
+        else:
+            for subelement in element:
+                self.parse_children(subelement, datamodel)
+
+    def prepare_datamodel(self):
+        '''
+        Create the initial parts of the Data Model
+        '''
+        self.datamodel['networks'] = DataModel()
+        self.datamodel['pdus'] = DataModel()
+        self.datamodel['signals'] = DataModel()
+        self.datamodel['compumethods'] = DataModel()
+        self.datamodel['frames'] = DataModel()
+
+    def checkarxml(self):
+        '''
+        check and validate the Ecu extract
+        '''
+        #for schema in schemas:
+        #if self.xmldoc.xmlschema(schemas[schema]):
+        #    get_parsedict(schema)
+        #else:
+        #    raise InvalidArxml('Schema validation failed')
+        return None
+
+    def get_nsmap(self):
+        '''
+        Obtain the namespace mapping
+        '''
+        nsmap = self.root.nsmap
+        nsmap['default'] = nsmap.pop(None)
+        return nsmap
+
